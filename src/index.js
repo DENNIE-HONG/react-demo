2e4c023512870aba082af0c0d96773c622c6096a
洪鹭燕 <luyanhong213010@sohu-inc.com>
2018-06-20 11:57:18 +0800

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App'; // 引入组件，在这个组件中进行热更新
+import App from './containers/App'; // 引入组件，在这个组件中进行热更新
 
 const root = document.createElement('div');
 document.body.appendChild(root);
-
 render(<App />, root);
